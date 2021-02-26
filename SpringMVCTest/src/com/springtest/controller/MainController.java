d2c4fac00287bc879b6c98c64287c92715459c8c
yondo123 <yondo123@gmail.com>
2021-02-26 17:28:58 +0900

    SpringMVCTest : xml 방식의 스프링 mvc 구조 프로젝트 셋팅

---
+++ SpringMVCTest/src/com/springtest/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.springtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String main() {
+		System.out.println("main Controller!!!!!");
+		return "index";
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		System.out.println("test url Controller!!!");
+		return null;
+	}
+}
