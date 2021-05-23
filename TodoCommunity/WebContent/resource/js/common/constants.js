40e51c9a16c9160339171ae503bf16a1cb7e6f3e
yondo123 <yondo123@gmail.com>
2021-05-23 21:38:49 +0900

    feat : 카테고리 리소스 경로 추가

--- TodoCommunity/WebContent/resource/js/common/constants.js
+++ TodoCommunity/WebContent/resource/js/common/constants.js
@@ -3,9 +3,20 @@ const constants = {
     REQUEST_URL         : "/TodoCommunity",
     LEFT_BOARD          : "스터디",
     RIGHT_BOARD         : "커뮤니티",
+    STUDY_BOARD_ID      : "study",
+    COMMUNITY_BOARD_ID  : "com",
     /** item count */
     BOARD_COUNT         : 7,
     MAINPAGE_BOARD_COUNT: 5,
     MAINPAGE_TODO_COUNT : 5,
-    PAGINATION_COUNT    : 5
+    PAGINATION_COUNT    : 5,
+    /** resource defined */
+    JAVA_ICON_FILE      : "Java_logo.png",
+    JS_ICON_FILE        : "JS_logo.png",
+    ETC_ICON_FILE       : "ETC_logo.png",
+    MOBILE_ICON_FILE    : "Mobile_logo.png",
+    PHP_ICON_FILE       : "Php_logo.png",
+    PYTHON_ICON_FILE    : "Python_logo.png",
+    SQL_ICON_FILE       : "Sql_logo.png",
+    HTML_ICON_FILE      : "Html_logo.png"
 }
