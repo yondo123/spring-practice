351e2db9e30f62c72c9bfa19f9a8582f7afd7731
SeungJin Choi <46988995+yondo123@users.noreply.github.com>
2021-08-13 23:46:11 +0900

    init : RestfulBoard Project

---
+++ RestfulBoard/src/main/java/com/restfulBoard/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.restfulBoard.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class AppConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    //root-context
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootAppConfig.class};
+    }
+
+    //servlet-context
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ServletAppConfig.class};
+    }
+
+    //DispatcherServlet
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    //filter 설정
+    @Override
+    protected Filter[] getServletFilters(){
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        return new Filter[] {encodingFilter};
+    }
+}
---
+++ RestfulBoard/src/main/java/com/restfulBoard/config/RootAppConfig.java
@@ -0,0 +1,7 @@
+package com.restfulBoard.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootAppConfig {
+}
