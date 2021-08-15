614fbc7542b71e1b4e9062306cb61e6f9d441d47
yondo123 <46988995+yondo123@users.noreply.github.com>
2021-08-15 23:42:22 +0900

    fix : db프로퍼티 수정

--- RestfulBoard/src/main/java/com/restfulBoard/config/ServletAppConfig.java
+++ RestfulBoard/src/main/java/com/restfulBoard/config/ServletAppConfig.java
@@ -2,9 +2,7 @@ package com.restfulBoard.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.validation.MessageCodesResolver;
@@ -14,16 +12,16 @@ import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
-import javax.activation.DataSource;
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.restfulBoard.conroller")
+@ComponentScan("com.restfulBoard.controller")
+@ComponentScan("com.restfulBoard.dao")
+@ComponentScan("com.restfulBoard.service")
 
-@PropertySource("/WEB-INF/properties/db.properties")
+@PropertySource("classpath:properties/db.properties")
 public class ServletAppConfig implements WebMvcConfigurer {
     @Value("${db.classname}")
     private String db_classname;
@@ -132,11 +130,11 @@ public class ServletAppConfig implements WebMvcConfigurer {
     }
 
     /**
-     * database 셋팅
+     * database 설정
      */
+    @Bean
     public BasicDataSource dataSource(){
         BasicDataSource source = new BasicDataSource();
-        System.out.println(db_url+"and"+db_password);
         source.setDriverClassName(db_classname);
         source.setUrl(db_url);
         source.setUsername(db_username);
