188fab9e19c525942f6e8eedbe1a37498fad6d19
yondo123 <46988995+yondo123@users.noreply.github.com>
2021-08-17 23:36:19 +0900

    feat : user 세션 생성

--- RestfulBoard/src/main/java/com/restfulBoard/config/RootAppConfig.java
+++ RestfulBoard/src/main/java/com/restfulBoard/config/RootAppConfig.java
@@ -1,7 +1,15 @@
 package com.restfulBoard.config;
 
+import com.restfulBoard.dto.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Configuration
 public class RootAppConfig {
+    @Bean("loginUser")
+    @SessionScope
+    public User user(){
+        return new User();
+    }
 }
