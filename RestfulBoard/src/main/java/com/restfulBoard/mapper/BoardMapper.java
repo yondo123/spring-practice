fa343360fe46dd4e53b1ceab57ce6e89d62b6491
yondo123 <46988995+yondo123@users.noreply.github.com>
2021-08-18 23:14:25 +0900

    fix : 시퀀스 auto increment 방식 변경

--- RestfulBoard/src/main/java/com/restfulBoard/mapper/BoardMapper.java
+++ RestfulBoard/src/main/java/com/restfulBoard/mapper/BoardMapper.java
@@ -2,10 +2,11 @@ package com.restfulBoard.mapper;
 
 import com.restfulBoard.dto.Board;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
 
 public interface BoardMapper {
-    @Insert("INSERT INTO BOARD_TBL(POST_UUID, USER_ID, POST_CONTEXT, POST_DATE, POST_TITLE)" +
-            "VALUES ((SELECT NEXTSEQ('BOARD')), #{writer}, #{context}, DATE_FORMAT(NOW(), '%Y%m%d'), #{title})")
+    @Insert("INSERT INTO BOARD_TBL(USER_ID, POST_CONTEXT, POST_DATE, POST_TITLE)" +
+            "VALUES (#{writer}, #{context}, DATE_FORMAT(NOW(), '%Y%m%d'), #{title})")
     void insertPost(Board board);
 
 }
